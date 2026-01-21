1ce1681cdbbb593cc7bf9e0d1984ad2770500510
unknown <akthermasuma2001@gmail.com>
2026-01-22 01:17:29 +0600

    connected to MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Habit Tracker Server Running');
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    await client.connect();
+    const habitsCollection = client.db("habitTrackerDB").collection("habits");
+
+    // Test route
+    app.get("/", (req, res) => {
+      res.send("Habit Tracker Server Running");
+    });
+
+    // Get latest 6 habits
+    app.get("/habits/featured", async (req, res) => {
+      const result = await habitsCollection
+        .find({ visibility: "public" })
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+      res.send(result);
+    });
+
+  } finally {}
+}
+run().catch(console.dir);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
